ef136db8b10745c0277f94c9c6234dd38f99cde6
Mitchell Wintrow <90mitchwintrow@gmail.com>
2022-02-25 12:27:23 -0700

    Finished prototypal queue. These are much easier than functional and functional shared!

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,27 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var instance = Object.create(queueMethods);
+  instance.storage = {};
+  instance.keyCounter = 0;
+  return instance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  'enqueue': function(val) {
+    this.storage[this.keyCounter] = val;
+    this.keyCounter++;
+  },
+  'dequeue': function() {
+    var dqdkeys = Object.keys(this.storage);
+    if (dqdkeys.length === 0) {
+      return 0;
+    }
+    var dqd = Object.values(this.storage)[0];
+    delete this.storage[dqdkeys[0]];
+    return dqd;
+  },
+  'size': function() {
+    return Object.keys(this.storage).length;
+  }
+};
 
 
