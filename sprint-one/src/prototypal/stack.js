9d362d028494ea8d449970557e346a6092fc91a7
Mitchell Wintrow <90mitchwintrow@gmail.com>
2022-02-25 12:18:42 -0700

    Finished prototypal stack

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,27 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var instance = Object.create(stackMethods);
+  instance.storage = {};
+  instance.keyCounter = 0;
+  return instance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  'push': function(val) {
+    this.storage[this.keyCounter] = val;
+    this.keyCounter++;
+  },
+  'pop': function() {
+    if (this.keyCounter === 0) {
+      return 0;
+    }
+    var deleted = this.storage[this.keyCounter - 1];
+    delete this.storage[this.keyCounter - 1];
+    this.keyCounter--;
+    return deleted;
+  },
+  'size': function() {
+    return this.keyCounter;
+  }
+};
 
 
