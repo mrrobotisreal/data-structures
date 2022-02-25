83103c5bd11b4b1b3c78feaf076e30efc18d9c84
Mitchell Wintrow <90mitchwintrow@gmail.com>
2022-02-25 12:52:23 -0700

    Finished pseudoclassical queue

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,23 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.keyCounter = 0;
 };
 
+Queue.prototype.enqueue = function(val) {
+  this.storage[this.keyCounter] = val;
+  this.keyCounter++;
+};
 
+Queue.prototype.dequeue = function() {
+  var dqdkeys = Object.keys(this.storage);
+  if (dqdkeys.length === 0) {
+    return 0;
+  }
+  var dqd = Object.values(this.storage)[0];
+  delete this.storage[dqdkeys[0]];
+  return dqd;
+};
+
+Queue.prototype.size = function() {
+  return Object.keys(this.storage).length;
+};
