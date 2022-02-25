1bcc5e8d8937fa6dd5a2b6437e8a803c75b4ce49
Mitchell Wintrow <90mitchwintrow@gmail.com>
2022-02-25 12:44:02 -0700

    Finished pseudoclassical stack

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,23 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.keyCounter = 0;
 };
 
+Stack.prototype.push = function(val) {
+  this.storage[this.keyCounter] = val;
+  this.keyCounter++;
+};
 
+Stack.prototype.pop = function() {
+  if (this.keyCounter === 0) {
+    return 0;
+  }
+  var deleted = this.storage[this.keyCounter - 1];
+  delete this.storage[this.keyCounter - 1];
+  this.keyCounter--;
+  return deleted;
+};
+
+Stack.prototype.size = function() {
+  return this.keyCounter;
+};
